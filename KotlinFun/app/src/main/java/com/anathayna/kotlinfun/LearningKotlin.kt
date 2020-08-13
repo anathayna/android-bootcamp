738b4ba306d9d7ddd5a3a5e7875c28fb7955fd78
anathayna <anathaynafranca@gmail.com>
2020-08-13 13:02:29 -0300

    adding math

--- KotlinFun/app/src/main/java/com/anathayna/kotlinfun/LearningKotlin.kt
+++ KotlinFun/app/src/main/java/com/anathayna/kotlinfun/LearningKotlin.kt
@@ -4,7 +4,14 @@ fun main(args:Array<String>) {
     var age: Int = 24
     var food: String = "sushi"
     var wallet: Int = 90
+    var pi: Double = 3.14
+    var radius: Int = 5
+    var c = pi.toInt() * radius * 2
+
+    wallet -= 20
 
     println(age)
     println(food)
+    println(wallet)
+    println(c)
 }
