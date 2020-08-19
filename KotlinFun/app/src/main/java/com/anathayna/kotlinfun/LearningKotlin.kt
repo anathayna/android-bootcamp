03b4915e3abc9de6f399bc4e710351fa0025e308
anathayna <anathaynafranca@gmail.com>
2020-08-19 18:00:54 -0300

    if statements & boolean

--- KotlinFun/app/src/main/java/com/anathayna/kotlinfun/LearningKotlin.kt
+++ KotlinFun/app/src/main/java/com/anathayna/kotlinfun/LearningKotlin.kt
@@ -2,6 +2,9 @@ package com.anathayna.kotlinfun
 
 fun main(args:Array<String>) {
     var age: Int = 24
+    var weight: Int = 65
+    var isLightOn: Boolean = false
+    var canRide: Boolean = age > 11
     var food: String = "sushi"
     var wallet: Int = 90
     var pi: Double = 3.14
@@ -14,4 +17,24 @@ fun main(args:Array<String>) {
     println(food)
     println(wallet)
     println(c)
+
+    if(isLightOn) {
+        println("walk across the room")
+    } else {
+        println("don't move a muscle")
+    }
+
+    // age > 11
+    if(canRide) {
+        println("enjoy the ride")
+    } else {
+        println("sorry son... maybe another day")
+    }
+
+    if(weight >= 80) {
+        println("slide on down!")
+    } else {
+        println("try out the kiddie pool")
+    }
+
 }
