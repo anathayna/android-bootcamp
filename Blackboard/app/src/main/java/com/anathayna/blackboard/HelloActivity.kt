248a45d703ceb536efb854d60a57aaa0f2371d8c
anathayna <anathaynafranca@gmail.com>
2020-08-26 20:14:08 -0300

    adding when statement

--- Blackboard/app/src/main/java/com/anathayna/blackboard/HelloActivity.kt
+++ Blackboard/app/src/main/java/com/anathayna/blackboard/HelloActivity.kt
@@ -2,10 +2,20 @@ package com.anathayna.blackboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hello.*
 
 class HelloActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hello)
+
+        btnFim.setOnClickListener {
+            var pronome = ""
+            when (rgPronome.checkedRadioButtonId) {
+                R.id.rbEla -> pronome = "Ela"
+                R.id.rbEle -> pronome = "Ele"
+                R.id.rbOutro -> pronome = "Neutro"
+            }
+        }
     }
 }
