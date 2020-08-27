c76cad4edf6599bb61f1f8cb9cc41be316676f8d
anathayna <anathaynafranca@gmail.com>
2020-08-26 22:40:36 -0300

    adding date picker

--- Blackboard/app/src/main/java/com/anathayna/blackboard/DateTimeActivity.kt
+++ Blackboard/app/src/main/java/com/anathayna/blackboard/DateTimeActivity.kt
@@ -1,11 +1,29 @@
 package com.anathayna.blackboard
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_date_time.*
+import java.util.*
 
 class DateTimeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_date_time)
+
+        val calendar = Calendar.getInstance()
+        val ano = calendar.get(Calendar.YEAR)
+        val mes = calendar.get(Calendar.MONTH)
+        val dia = calendar.get(Calendar.DAY_OF_MONTH)
+
+        etData.setOnClickListener {
+            DatePickerDialog(
+                this,
+                { view, day, mouth, year ->
+                    etData.setText("$day/$mouth/$year")
+                },
+                dia, mes, ano
+            ).show()
+        }
     }
 }
