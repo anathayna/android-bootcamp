e6a2e1e8b0a52679ea564aeb337eb041d98ca7b2
anathayna <anathaynafranca@gmail.com>
2020-08-26 21:43:45 -0300

    finish contact code

--- Blackboard/app/src/main/java/com/anathayna/blackboard/ContactActivity.kt
+++ Blackboard/app/src/main/java/com/anathayna/blackboard/ContactActivity.kt
@@ -2,10 +2,30 @@ package com.anathayna.blackboard
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_contact.*
 
 class ContactActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_contact)
+
+        btnConcluir.setOnClickListener {
+            var msg = """Nome: ${etNome.text}
+                |Telefone: ${etTel.text}
+                |Email: ${etEmail.text}
+                |
+                |Preferência de Contato
+            """.trimMargin("|")
+
+            if (cbTel.isChecked) {
+                msg += "\n - Telefone"
+            }
+
+            if (cbEmail.isChecked) {
+                msg += "\n - Email"
+            }
+
+            alert("Dados", msg, this)
+        }
     }
 }
