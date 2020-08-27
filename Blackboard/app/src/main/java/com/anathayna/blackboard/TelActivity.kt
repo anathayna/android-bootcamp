0fe896dca7f313fdb2c0fce67ac3e5fc55819e41
anathayna <anathaynafranca@gmail.com>
2020-08-26 22:17:13 -0300

    finish finish tel code

--- Blackboard/app/src/main/java/com/anathayna/blackboard/TelActivity.kt
+++ Blackboard/app/src/main/java/com/anathayna/blackboard/TelActivity.kt
@@ -3,7 +3,11 @@ package com.anathayna.blackboard
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import kotlinx.android.synthetic.main.activity_contact.*
 import kotlinx.android.synthetic.main.activity_tel.*
+import kotlinx.android.synthetic.main.activity_tel.btnConcluir
+import kotlinx.android.synthetic.main.activity_tel.etNome
+import kotlinx.android.synthetic.main.activity_tel.etTel
 
 class TelActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -16,5 +20,19 @@ class TelActivity : AppCompatActivity() {
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
 
         spTipo.adapter = adapter
+
+        btnConcluir.setOnClickListener {
+            if (!etNome.text.isEmpty() && !etTel.text.isEmpty() && spTipo.selectedItemPosition != 0) {
+                val msg = """Nome: ${etNome.text}
+                |Telefone: ${etTel.text}
+                |Tipo de Telefone: ${spTipo.selectedItem}
+                """.trimMargin("|")
+
+                alert("Concluído", msg, this)
+            }
+            else {
+                alert("Erro", "Preencha todos os campos", this)
+            }
+        }
     }
 }
