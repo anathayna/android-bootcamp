79639bfb70112cd455e88fd9cefd4358a9fe99eb
anathayna <anathaynafranca@gmail.com>
2020-09-02 20:46:51 -0300

    adding send params example

---
+++ Imagine/app/src/main/java/com/anathayna/imagine/ExtraExActivity.kt
@@ -0,0 +1,11 @@
+package com.anathayna.imagine
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExtraExActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_extra_ex)
+    }
+}
