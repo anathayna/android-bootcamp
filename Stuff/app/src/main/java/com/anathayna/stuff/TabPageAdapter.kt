476086bb3bd3788100c95f50a5a9a7779ff4700e
anathayna <anathaynafranca@gmail.com>
2020-09-23 20:51:00 -0300

    finish tab adapter class

--- Stuff/app/src/main/java/com/anathayna/stuff/TabPageAdapter.kt
+++ Stuff/app/src/main/java/com/anathayna/stuff/TabPageAdapter.kt
@@ -1,4 +1,19 @@
 package com.anathayna.stuff
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.adapter.FragmentStateAdapter
 
-class TabPageAdapter {
+class TabPageAdapter(f: FragmentActivity): FragmentStateAdapter(f) {
+    override fun getItemCount(): Int {
+        return 3
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when (position) {
+            1 -> return AlbumsFragment.newInstance()
+            2 -> return ArtistsFragment.newInstance()
+            else -> return RecentsFragment.newInstance()
+        }
+    }
+
 }
