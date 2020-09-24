abe8bc116d43195ebb5668186ea67c61e5ed82f1
anathayna <anathaynafranca@gmail.com>
2020-09-23 22:04:29 -0300

    finish for today because my internet sucks

---
+++ Stuff/app/src/main/java/com/anathayna/stuff/BottomNavigationActivity.kt
@@ -0,0 +1,11 @@
+package com.anathayna.stuff
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BottomNavigationActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bottom_navigation)
+    }
+}
--- Stuff/app/src/main/java/com/anathayna/stuff/MainActivity.kt
+++ Stuff/app/src/main/java/com/anathayna/stuff/MainActivity.kt
@@ -2,10 +2,26 @@ package com.anathayna.stuff
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.tabs.TabLayoutMediator
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = TabPageAdapter(this)
+
+        viewPager.adapter = adapter
+
+        TabLayoutMediator(tabLayout, viewPager) { tab, position ->
+            when (position) {
+                0 -> tab.text = "recents"
+                1 -> tab.text = "albums"
+                2 -> tab.text = "artists"
+            }
+        }.attach()
     }
+
 }
