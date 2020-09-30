1a5047343cfa86cfa556b8e6da41aa16230f5b8a
anathayna <anathaynafranca@gmail.com>
2020-09-30 20:09:25 -0300

    add notes project

---
+++ Notes/app/src/main/java/com/anathayna/notes/activities/ListNotesActivity.kt
@@ -0,0 +1,12 @@
+package com.anathayna.notes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anathayna.notes.R
+
+class ListNotesActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_list_notes)
+    }
+}
---
+++ Notes/app/src/main/java/com/anathayna/notes/activities/NewNoteActivity.kt
@@ -0,0 +1,12 @@
+package com.anathayna.notes.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.anathayna.notes.R
+
+class NewNoteActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_new_note)
+    }
+}
